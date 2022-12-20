d4e3ef2df63eff5ca0586804ec869886ad1db05e
Varunc99 <104201944+Varunc99@users.noreply.github.com>
2022-12-20 12:54:09 +0530

    Add files via upload

---
+++ Task14/Task14.py
@@ -0,0 +1,26 @@
+from flask import Flask,Response,request
+import json
+from connect2db import *
+app = Flask(__name__)
+base = connect_db()
+cur = base.cursor()
+@app.route('/workload',methods =['GET'])
+def workload():#get bench marks from database table
+    try:
+        l = []
+        cur.execute("select  distinct(workload) from cpu where is_workload_data = 1 order by workload ")
+        data=cur.fetchall()
+        print(data)
+        for i in data:
+            if i[0] not in l:
+                l.append(i[0])
+        resp_data = {"message": "successful!", "workload": l}
+        response = json.dumps(resp_data)
+        response = Response(response, status=200, mimetype='application/json')
+        return response
+    except Exception as e:
+        print(f"\n{'='*30}\n{e}\n{'='*30}\n")
+        error = {"error": str(e)}
+        return jsonify(error)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
