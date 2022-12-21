dd0757c51a422a63257fd9c782d99344303a325e
Arbind Kumar <arbindkumar.dbdamarch22@gmail.com>
2022-12-21 13:44:01 +0530

    task files

---
+++ workload_profiler_arbind/start_end_dates_native9.py
@@ -0,0 +1,31 @@
+from flask import Flask,Response,request
+import json
+from connect2db import *
+app = Flask(__name__)
+base = connect_db()
+cur = base.cursor()
+@app.route('/cpu',methods =['GET'])
+def start_endtime(): #to get start and end dates of selected native workload and get the response in json forma
+    try:
+        l = {}
+        cur.execute(
+            "select host,date(min(timestamp)) as start_date,date(max(timestamp)) as End_date from cpu group by host;")
+        data = cur.fetchall()
+        for i in data:
+            l.update({str(i[0]): [str(i[1]), str(i[2])]})
+
+        resp_data = {"Timestamp": l}
+        response = json.dumps(resp_data)
+        response = Response(response, status=200, mimetype='application/json')
+        return response
+    except Exception as e:
+        error = {"error" : "Connection with database is failed"}
+        print(f"\n{'=' * 30}\n{e}\n{'=' * 30}\n")
+        print(f"\n{'=' * 30}\n{error}\n{'=' * 30}\n")
+        return jsonify(error)
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
